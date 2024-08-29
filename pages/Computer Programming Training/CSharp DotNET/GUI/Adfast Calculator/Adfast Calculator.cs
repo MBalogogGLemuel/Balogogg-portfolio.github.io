dc638b02357cc13dbd4729e148021532b93ab307
MBalogogGLemuel <georges.balogog@yahoo.fr>
2024-08-28 20:05:32 -0400

    Update
    Upload des fichiers dotNet

---
+++ pages/Computer Programming Training/CSharp DotNET/GUI/Adfast Calculator/Adfast Calculator.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: georg
+ * Date: 2024-08-17
+ * Time: 5:36 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AdFast_Calculator
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button14Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Label2Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Label3Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Label5Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
